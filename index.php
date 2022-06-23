8b7920b7ae3ca861a7557caf72c44d25a76af5be
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-23 16:58:41 +0200

    add a litle bit of style

--- index.php
+++ index.php
@@ -30,6 +30,7 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="style.css">
+   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,300&display=swap" rel="stylesheet">
    <title>Document</title>
 </head>
 <body>
